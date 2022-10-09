7ba6864700b40d51dee8d4ad8dfd2962ca4bff41
Mojeeto <mojtaba01nouri@gmail.com>
2022-10-09 17:51:23 +0330

    tailwind css framework

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      Tailwind <span className="text-green-500">installed</span>
+    </div>
+  );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-root.render(<div>System is ok</div>);
+root.render(<App />);
